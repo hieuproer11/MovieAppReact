a2cb35e619e421021c200088056758bf4ec3add7
hieuproer11 <quanghieu08062000@gmail.com>
2025-10-21 05:28:09 +0200

    first commit

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,12 @@
+import '../css/Favorites.css'
+
+function Favorites() {
+    return (
+        <div className="favorites-empty">
+            <h2>No favorites Movies yet</h2>
+            <p>Build your own favorite album of movies now! </p>
+        </div>
+    )
+}
+
+export default Favorites
---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY ="3e7c8d765b53c27b81f3f59f34d1cb8f";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+
+export const searchMovies = async () => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json()
+    return data.results
+};
